1489396e587d98804f8ef483397865b64dc4a1ba
jeangathoni8 <jeangathoni39@gmail.com>
2024-06-03 10:06:52 +0300

    "Added SQL table creation and insertion scripts, and implemented SQL queries in python file"

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+(NULL, 3, 'M', 'brown', 'aggressive', 1),
+('Teddy', 10, 'M', 'black', 'calm', 1),
+('Grizzly', 5, 'M', 'brown', 'aggressive', 0),
+('Baloo', 8, 'M', 'blue', 'friendly', 1),
+('Paddington', 6, 'M', 'brown', 'calm', 1),
+('Winnie', 4, 'M', 'yellow', 'friendly', 1),
+('Yogi', 7, 'M', 'brown', 'mischievous', 1),
+('Smokey', 9, 'M', 'black', 'calm', 1);
